98659c5e0669b8968f4fd8a47a03c00164f48b39
dinismaksim22-sketch <dinismaksim22@gmail.com>
2026-04-09 20:32:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,6 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "⏳ Ожидайте, все объявления проверяются модераторами, не нужно дублировать сообщение.")
+    bot.send_message(message.chat.id, "Привет! Я твой бот 🤖")
 
 bot.infinity_polling()
